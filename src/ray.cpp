3af92465a1e4510acc40b356d0998679ea16807a
San7o <santigio2003@gmail.com>
2025-12-22 23:56:55 +0100

    raytracer: vec3 class

---
+++ src/ray.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "vec3.hpp"
+
+class ray
+{
+public:
+  
+  ray() {}
+  ray(const point3& origin, const vec3& direction)
+    : orig(origin), dir(direction) {}
+
+  const point3& origin() const { return this->orig; }
+  const vec3& direction() const { return this->dir; }
+
+  point3 at(double t) const
+  {
+    return orig + t * dir;
+  }
+
+private:
+
+  point3 orig;
+  vec3 dir;
+  
+};
