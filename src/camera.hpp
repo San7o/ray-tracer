776d14a37526b21d5f398735d2c69ba3b95af89c
San7o <santigio2003@gmail.com>
2026-02-17 11:46:27 +0100

    raytracer: diffuse materials

--- src/camera.hpp
+++ src/camera.hpp
@@ -13,6 +13,7 @@ public:
   double aspect_ratio  = 1.0;
   int    image_width   = 100;
   int    samples_per_pixel = 10;  // count of random samples of each pixel
+  int    max_depth     = 10;     // maximum number of ray bonces into scene
 
   
   void render(const hittable& world)
@@ -30,7 +31,7 @@ public:
         for (int sample = 0; sample < samples_per_pixel; sample++)
         {
           ray r = get_ray(i, j);
-          pixel_color += ray_color(r, world);
+          pixel_color += ray_color(r, max_depth, world);
         }
         
         write_color(std::cout, pixel_samples_scale * pixel_color);
@@ -125,12 +126,30 @@ private:
     return vec3(random_double() - 0.5, random_double() - 0.5, 0);
   }
 
-  color ray_color(const ray& r, const hittable& world) const
+  color ray_color(const ray& r, int depth, const hittable& world) const
   {
+    if (depth <= 0) // limit recursion
+      return color(0, 0, 0);
+    
     hit_record rec;
-    if (world.hit(r, interval(0, infinity), rec))
+    if (world.hit(r, interval(0.001, infinity), rec))
+      // we use 0.001 to account for floating point inaccuracies
     {
-      return 0.5 * (rec.normal + color(1, 1, 1));
+      // Pick a direction according to lambertian distribution
+      //
+      // This distribution scatters reflected rays in a manner that is
+      // proportional to cos(phi), where phi is the angle between the
+      // reflected ray and the surface normal. This means that a
+      // reflected ray is most likely to scatter in a direction near
+      // the surface normal, and less likely to scatter in directions
+      // away from the normal.
+      // We do this by generating a sphere tangent to the hit point
+      // and randmly selecting a point on its surface for the
+      // direction
+      vec3 direction = rec.normal + random_unit_vector();
+
+      double reflectance = 0.7;
+      return reflectance * ray_color(ray(rec.p, direction), depth-1, world);
     }
 
     // Blend white and blue depending on the height of y (after
